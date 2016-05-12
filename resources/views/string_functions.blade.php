4c5a8f9bceed81e3a76ea4e3b86ef8d95747bc96
manidf <mannuel.ferreira@gmail.com>
2016-05-12 08:34:06 +0200

    more strings

--- resources/views/string_functions.blade.php
+++ resources/views/string_functions.blade.php
@@ -1,5 +1,5 @@
 	<?php
-		$firstString = "The quick brown fox";
+		$firstString = "The quick brown fox ";
 		$secondString = "jumped over the lazy dog.";
 	?>
 
@@ -8,3 +8,14 @@
 		$thirdString .= $secondString;
 		echo $thirdString;
 	?>
+	<br/>
+	Lowercase: <?php echo strtolower($thirdString); ?> <br>
+	Uppercase: <?php echo strtoupper($thirdString); ?> <br>
+	Uppercase: first-letter: <?php echo ucfirst($thirdString); ?> <br>
+	Uppercase words: <?php echo ucwords($thirdString); ?> <br>
+
+	<br>
+	Length: <?php echo strlen($thirdString); ?> <br>
+	Trim: <?php echo $fourthString = $firstString . trim($secondString); ?> <br>
+	Find: <?php echo strstr($thirdString, "brown"); ?> <br>
+	Replace by string: <?php echo str_replace("quick", "super-fast", $thirdString); ?>
