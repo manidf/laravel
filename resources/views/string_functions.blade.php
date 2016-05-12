ce54a07562edf8e354bf7081534e05009c0c2a1a
manidf <mannuel.ferreira@gmail.com>
2016-05-12 08:48:38 +0200

    more strings

--- resources/views/string_functions.blade.php
+++ resources/views/string_functions.blade.php
@@ -8,7 +8,7 @@
 		$thirdString .= $secondString;
 		echo $thirdString;
 	?>
-	<br/>
+	<br>
 	Lowercase: <?php echo strtolower($thirdString); ?> <br>
 	Uppercase: <?php echo strtoupper($thirdString); ?> <br>
 	Uppercase: first-letter: <?php echo ucfirst($thirdString); ?> <br>
@@ -18,4 +18,10 @@
 	Length: <?php echo strlen($thirdString); ?> <br>
 	Trim: <?php echo $fourthString = $firstString . trim($secondString); ?> <br>
 	Find: <?php echo strstr($thirdString, "brown"); ?> <br>
-	Replace by string: <?php echo str_replace("quick", "super-fast", $thirdString); ?>
+	Replace by string: <?php echo str_replace("quick", "super-fast", $thirdString); ?> <br>
+
+	<br>
+	Repeat: <?php echo str_repeat($thirdString, 2); ?> <br>
+	Make substring: <?php echo substr($thirdString, 5, 10); ?> <br>
+	Find position: <?php echo strpos($thirdString, "brown"); ?> <br>
+	Find character: <?php echo strchr($thirdString, "z"); ?> <br>
